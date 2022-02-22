a9351b975bfff1aa04aa0b6e5d88d53cc4b781aa
Facundo A. Bértoli <fbertoli@vi-datec.com>
2022-02-22 18:22:24 -0300

    fix posts.controller, create services post.services.js

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,22 +1,15 @@
 const { response } = require('express');
-const axios = require('axios');
-const { paginate } = require('../helpers/validators');
+const PostService = require('../services/post.services');
 
 const getPosts = async (req = request, res = response) => {
+  const { perPage, page } = req.query;
   try {
-    const { perPage, page } = req.query;
-    const { data } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    posts = [];
-    posts = data;
-    totalPosts = posts.length;
-    result = await paginate(posts, page, perPage);
-    res.json({
-      result,
-    });
-  } catch (error) {
-    console.log(error);
+    const { result } = await PostService.getPosts(perPage, page);
+    return res
+      .status(200)
+      .json({ status: 200, result, message: 'Listado finalizado' });
+  } catch (e) {
+    return res.status(404).json({ status: 404, message: e.message });
   }
 };
 
