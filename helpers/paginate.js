8c63a25115e61404f36351f9993c73f86864e5fb
bertolifacundo <32556069+bertolifacundo@users.noreply.github.com>
2022-02-24 12:53:53 -0300

    Update paginate.js

--- helpers/paginate.js
+++ helpers/paginate.js
@@ -1,12 +1,13 @@
-const paginate = async (items, page = 1) => {
+const paginate = async (items, page = 0) => {
   const validPageAndPerPage = Number.isInteger(Number(page));
   if (!validPageAndPerPage) {
     throw new Error('Parametros incorrectos');
   }
+  const pageNumber = Number(page);
   const paginatedItems = items;
   return {
-    previousPage: Number(page) - 1 ? Number(page) - 1 : null,
-    nextPage: Number(page) ? Number(page) + 1 : null,
+    previousPage: pageNumber > 0 ? pageNumber - 1 : 0,
+    nextPage: pageNumber ? pageNumber + 1 : 0,
     total: items.length,
     items: paginatedItems,
   };
